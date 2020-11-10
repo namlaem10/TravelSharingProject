375f788c35fba02ca38f8e07b0c821bbc42fdb68
hien-le <toan-hien.le-vu@ant-tech.eu>
2020-11-10 21:16:54 +0700

    - Adding function to handle message input and slice it into words and count the time they appear

--- src/Functions/Corpus/corpusWord.js
+++ src/Functions/Corpus/corpusWord.js
@@ -2,5 +2,17 @@ const regEx = '([a-zA-Z]w+)W*';
 
 export const corpusFunction = messages => {
   let collection = [];
-  console.log(messages[0].text, '==========MESSAGE');
+  let wordArray = messages[0].text.split(' ');
+  for (let i = 0; i < wordArray.length; i++) {
+    collection.push({key: wordArray[i], value: 0});
+  }
+
+  collection.forEach(item => {
+    for (let i = 0; i < wordArray.length; i++) {
+      if (wordArray[i] == item.key) {
+        item.value++;
+      }
+    }
+  });
+  console.log(collection, '====================== COLLECTION');
 };
